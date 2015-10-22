17ddd4350a42c805879b93f50588a4636acf20c2
Carlos Garnacho <carlosg@gnome.org>
2015-10-22 14:07:52 +0200

    wayland: Add MetaWaylandSurface::destroy signal
    There's places where it would be convenient to add listeners to this,
    so add the signal. The signal is only emitted once during destruction,
    it is convenient for the places where we want notifications at a time
    the object is still alive, as opposed to weak refs which notify after
    the fact.

--- src/wayland/meta-wayland-surface.c
+++ src/wayland/meta-wayland-surface.c
@@ -124,6 +124,13 @@ G_DEFINE_TYPE (MetaWaylandSurfaceRoleDND,
                meta_wayland_surface_role_dnd,
                META_TYPE_WAYLAND_SURFACE_ROLE);
 
+enum {
+  SURFACE_DESTROY,
+  N_SURFACE_SIGNALS
+};
+
+guint surface_signals[N_SURFACE_SIGNALS] = { 0 };
+
 static void
 meta_wayland_surface_role_assigned (MetaWaylandSurfaceRole *surface_role);
 
@@ -2407,6 +2414,20 @@ meta_wayland_surface_get_toplevel_window (MetaWaylandSurface *surface)
 }
 
 static void
+meta_wayland_surface_dispose (GObject *object)
+{
+  MetaWaylandSurface *surface = META_WAYLAND_SURFACE (object);
+
+  if (!surface->destroying)
+    {
+      g_signal_emit (object, surface_signals[SURFACE_DESTROY], 0);
+      surface->destroying = TRUE;
+    }
+
+  G_OBJECT_CLASS (meta_wayland_surface_parent_class)->dispose (object);
+}
+
+static void
 meta_wayland_surface_init (MetaWaylandSurface *surface)
 {
 }
@@ -2414,6 +2435,17 @@ meta_wayland_surface_init (MetaWaylandSurface *surface)
 static void
 meta_wayland_surface_class_init (MetaWaylandSurfaceClass *klass)
 {
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->dispose = meta_wayland_surface_dispose;
+
+  surface_signals[SURFACE_DESTROY] =
+    g_signal_new ("destroy",
+                  G_TYPE_FROM_CLASS (object_class),
+                  G_SIGNAL_RUN_LAST,
+                  0, NULL, NULL,
+                  g_cclosure_marshal_VOID__VOID,
+                  G_TYPE_NONE, 0);
 }
 
 static void
